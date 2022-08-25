9262d3ecac9ea1bb4b352271ec8f1d6404785b38
Qweku <brobbeycollins96@gmail.com>
2022-08-25 07:39:02 +0000

    home changes

--- lib/myHome.dart
+++ lib/myHome.dart
@@ -211,10 +211,10 @@ class _SpeechScreenState extends State<SpeechScreen> {
               Positioned(
                   bottom: 0,
                   left: 0,
-                  right: 0,
+                  //right: 0,
                   child: Container(
-                    height:height*0.05,
-                    width: width*0.8,
+                    height:50,
+                    //width: width*0.8,
                     //color: Colors.white,
                     child: AdWidget(ad:bottomAd),
                   ))
